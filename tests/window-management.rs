91474504b6a5353445ddbc6fa14fc423459b0abd
Piyush Raj <hello@piyushraj.org>
2026-06-15 23:55:12 +0530

    test: jerry-rigged window state management integration

--- tests/window-management.rs
+++ tests/window-management.rs
@@ -3,6 +3,7 @@ fn main() {
         .start()
         .unwrap();
 
+    // Visible immediately
     runtime.create_window(kurogane::WindowOptions {
         url: "https://en.wikipedia.org/wiki/Rust_(programming_language)".into(),
         bounds: kurogane::BrowserBounds {
@@ -14,6 +15,7 @@ fn main() {
         show_state: kurogane::WindowState::Normal,
     }).unwrap();
 
+    // Starts maximized
     runtime.create_window(kurogane::WindowOptions {
         url: "https://github.com/0x48piraj/kurogane".into(),
         bounds: kurogane::BrowserBounds {
@@ -22,7 +24,31 @@ fn main() {
             width: 800,
             height: 600,
         },
-        show_state: kurogane::WindowState::Normal,
+        show_state: kurogane::WindowState::Maximized,
+    }).unwrap();
+
+    // Starts minimized
+    runtime.create_window(kurogane::WindowOptions {
+        url: "https://www.rust-lang.org".into(),
+        bounds: kurogane::BrowserBounds {
+            x: 360,
+            y: 270,
+            width: 800,
+            height: 600,
+        },
+        show_state: kurogane::WindowState::Minimized,
+    }).unwrap();
+
+    // Starts hidden
+    runtime.create_window(kurogane::WindowOptions {
+        url: "https://docs.rs".into(),
+        bounds: kurogane::BrowserBounds {
+            x: 480,
+            y: 360,
+            width: 800,
+            height: 600,
+        },
+        show_state: kurogane::WindowState::Hidden,
     }).unwrap();
 
     while !runtime.should_shutdown() {
